19347c2dd43fb283aef16a007c481cb59640e4ae
joshvasquezr <josh.vasquez.ruiz@gmail.com>
2024-10-11 15:45:10 -0700

    added _05_Memory.java

--- src/main/java/com/example/_05_Memory.java
+++ src/main/java/com/example/_05_Memory.java
@@ -17,4 +17,59 @@ import static dev.langchain4j.data.message.UserMessage.userMessage;
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 
 public class _05_Memory {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        OpenAiStreamingChatModel model = OpenAiStreamingChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName("GPT_4_O_MINI")
+                .build();
+
+        ChatMemory chatMemory = TokenWindowChatMemory.withMaxTokens(1000, new OpenAiTokenizer());
+
+        SystemMessage systemMessage = SystemMessage.from(
+                "You are a senior developer explaining to another senior developer, "
+                + "the project you are working on is an e-commerce platform with a Java back-end, "
+                + "Oracle database, and Spring Data JPA"
+        );
+
+        UserMessage userMessage1 = userMessage(
+                "How do I optimize database queries for a large-scale e-commerce platform? "
+                + "Answer short in three to five lines maximum."
+        );
+        chatMemory.add(userMessage1);
+
+        System.out.println("[User]: " + userMessage1.text());
+        System.out.println("[LLM]: ");
+
+        CompletableFuture<AiMessage> futureAiMessage = new CompletableFuture<>();
+
+        StreamingResponseHandler<AiMessage> handler = new StreamingResponseHandler<AiMessage>() {
+            @Override
+            public void onNext(String token) {
+                System.out.println(token);
+            }
+
+            @Override
+            public void onComplete(Response<AiMessage> response) {
+                futureAiMessage.complete(response.content());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+            }
+        };
+
+        model.generate(chatMemory.messages(), handler);
+        chatMemory.add(futureAiMessage.get());
+
+        UserMessage userMessage2 = userMessage(
+                "Give a concrete example implementation of the first point? " +
+                        "Be short, 10 lines of code maximum.");
+        chatMemory.add(userMessage2);
+
+        System.out.println("\n\n[User]: " + userMessage2.text());
+        System.out.println("[LLM]: ");
+
+        model.generate(chatMemory.messages(), handler);
+    }
 }
