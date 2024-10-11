2df0efe97b1baff2a110022f4e1c6366aa66d7fd
joshvasquezr <59066378+joshvasquezr@users.noreply.github.com>
2024-10-10 20:50:41 -0700

    added two more example files... _05_ import dev.langcgain4j.memory.chat.TokenWindowChatMemory not working

---
+++ src/main/java/com/example/_04_Streaming.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.model.StreamingResponseHandler;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import dev.langchain4j.model.output.Response;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+
+public class _04_Streaming {
+
+    public static void main(String[] args) {
+
+        OpenAiStreamingChatModel model = OpenAiStreamingChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(GPT_4_O_MINI)
+                .build();
+
+        String prompt = "Write a short funny poem about developers and null-pointers, 10 lines maximum";
+
+        System.out.println("Nr of chars: " + prompt.length());
+        System.out.println("Nr of tokens: " + model.estimateTokenCount(prompt));
+
+        model.generate(prompt, new StreamingResponseHandler<AiMessage>(){
+
+            @Override
+            public void onNext(String token) {
+                System.out.println(token);
+            }
+
+            @Override
+            public void onComplete(Response<AiMessage> response) {
+                System.out.println("\n\nDone streaming");
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                System.out.println("Something went wrong: " + error.getMessage());
+            }
+        });
+    }
+}
---
+++ src/main/java/com/example/_05_Memory.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.TokenWindowChatMemory;
+import dev.langchain4j.model.StreamingResponseHandler;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import dev.langchain4j.model.openai.OpenAiTokenizer;
+import dev.langchain4j.model.output.Response;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static dev.langchain4j.data.message.UserMessage.userMessage;
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+
+public class _05_Memory {
+}
